11c7e65d24588db1a606ac1d81f90babf143c873
oltanklo <tankz0r@msn.com>
2015-04-10 11:12:53 -0500

    Updated js

--- js/mp3.js
+++ js/mp3.js
@@ -1,7 +1,6 @@
 $(function() {
   var $mp3Expand = $('#mp3_listbox');
-  $mp3Expand.hide();
-  
+  $mp3Expand.hide();  
   $('#mp3_expand').click(function() {
     $mp3Expand.stop().slideToggle(500);
   });
